ef7be14830043b01f3b721e1178ef865074d1f85
fterrani <fabien.terrani@students.hevs.ch>
2018-11-07 22:12:49 +0100

    Testing another username for wp-config.php

--- docker_wordpress/wp_files/wp-config.php
+++ docker_wordpress/wp_files/wp-config.php
@@ -22,7 +22,7 @@
 define('DB_NAME', 'db_wp_chanter');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'root');
+define('DB_USER', 'whatever');
 
 /** Mot de passe de la base de données MySQL. */
 define('DB_PASSWORD', 'AdminHevs01');
